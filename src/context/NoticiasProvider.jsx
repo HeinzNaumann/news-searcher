a7387c3e8699fd0a367f1a0e34960a26041841a4
Heinz <hlanaumann@gmail.com>
2022-05-05 14:52:04 +0200

    added the first changes

---
+++ src/context/NoticiasProvider.jsx
@@ -0,0 +1,25 @@
+import { useState, useState, createContext } from 'react'
+
+const NoticiasContext = createContext()
+
+const NoticiasProvider = ({ children }) => {
+
+    return (
+
+        <NoticiasContext.Provider
+
+            value={{}}
+        >
+
+
+            {children}
+
+        </NoticiasContext.Provider>
+    )
+}
+
+export {
+    NoticiasProvider
+}
+
+export default NoticiasContext
